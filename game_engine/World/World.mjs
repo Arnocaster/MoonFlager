3e7ec017bbd787ba08e68fee865efe18b52e4a16
Arnaud Peybernes <83549834+Arnocaster@users.noreply.github.com>
2022-01-23 23:08:47 +0100

    updateWorld improvment

--- game_engine/World/World.mjs
+++ game_engine/World/World.mjs
@@ -6,12 +6,12 @@ export default class World {
   get settings() {
     return this.#settings;
   }
-  get actionsBuffer(){
+  get actionsBuffer() {
     return this.#actionsBuffer;
   }
 
   constructor() {
-    this.id = parseInt(Math.random()*500000);
+    this.id = parseInt(Math.random() * 500000);
     this.entities = new Entities;
 
     this.#settings = {
@@ -23,33 +23,46 @@ export default class World {
     this.#actionsBuffer = [];
   }
 
-  addPlayer(id){
-    if (id){
+  addPlayer(id) {
+    if (id) {
       this.entities.add(new Player(id));
       return
     }
     console.error('You need an Id to create a player');
   }
 
-  removePlayer(id){
-    if(id){
+  removePlayer(id) {
+    if (id) {
       this.entities.remove(id);
       return
     }
     console.error('You need an Id to delete a player');
   }
 
-  updateWorld(newWorld){
+  updateWorld(newWorld) {
     const oldWorld = this.entities.get();
-    if (newWorld){
-      Object.keys(newWorld).forEach(type =>{
+    if (newWorld) {
+      Object.keys(newWorld).forEach(type => {
         newWorld[type].forEach(entity => {
-          const thisClass = new this.entities.classes[type];
-          this.entities.add(thisClass,entity);
-          console.log(this.entities.get());
+          if (Object.keys(oldWorld).length<1){
+            const thisClass = new this.entities.classes[type];
+            this.entities.add(thisClass, entity);
+            console.log('length');
+          }
+          if (oldWorld[type]) {
+            const found = oldWorld[type].find(oldEnt => oldEnt.id === entity.id);
+            if (!found) {
+              const thisClass = new this.entities.classes[type];
+              this.entities.add(thisClass, entity);
+              console.log('!found');
+            }
+          }
+
+
+
         });
       });
-      
+
     }
     //Update each entitie;
     return this.entities.get();
