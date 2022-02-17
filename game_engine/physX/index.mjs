8161da840cc67fa35a70a0dacae10127d4145e3c
Arnaud Peybernes <83549834+Arnocaster@users.noreply.github.com>
2022-02-17 22:20:27 +0100

    Physix crud schema

--- game_engine/physX/index.mjs
+++ game_engine/physX/index.mjs
@@ -1,10 +1,28 @@
 export default class physicEngine {
   constructor(PhysX){
+    this.gravity = '9.1 || 1.6';
     this.init(PhysX);
   }
   async init(PhysX) {
     this.physx = await PhysX();
     console.log(this.physx);
+  }
+
+  create(){
+    //Add an element to physic simulation
+  }
+
+  destroy(){
+    //Delete an element from physic simulation;
+  }
+
+  update(){
+    //Apply action to physic engine;
+    //return world;
+  }
+
+  shutdown(){
+    //Destroy all physics elements
   }
 
 }
